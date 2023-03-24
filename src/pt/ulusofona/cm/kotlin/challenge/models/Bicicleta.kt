a74bb764c958fa67aaf5fe59671a92e4ede501f9
João Antas <a22002629@alunos.ulht.pt>
2023-03-24 16:20:49 +0000

    Commit Erro posicao

--- src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
@@ -1,6 +1,4 @@
 package pt.ulusofona.cm.kotlin.challenge.models
-
-import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import java.text.SimpleDateFormat
 
 class Bicicleta(identificador: String) : Veiculo(identificador) {
--- src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
@@ -4,9 +4,12 @@ import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
 
 class Posicao(var x: Int, var y: Int) {
     fun alterarPosicaoPara(x: Int, y: Int) {
-        this.x = x
-        this.y = y
-        throw AlterarPosicaoException()
+        if(this.x != x || this.y != y){
+            this.x = x
+            this.y = y
+        } else {
+            throw AlterarPosicaoException()
+        }
     }
 
     override fun toString(): String {
